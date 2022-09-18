be24641b6a09621966fcaf723f56834d942abef7
Magne Alvheim <magne@alvheim.com>
2022-09-18 18:25:16 +0200

    Cpp fungerer

--- Lab2/CPP/source/HelloWorld.cpp
+++ Lab2/CPP/source/HelloWorld.cpp
@@ -2,13 +2,13 @@
   
 // Header file for input output functions
 #include <iostream>
-using namespace std;
+
   
 // Main() function: where the execution of program begins
 int main()
 {
     // prints hello world
-    cout << "Hello World";
+    std::cout << "Hello World\n\n";
   
     return 0;
 }
