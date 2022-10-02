868c9c007fc98559faaf2857b409949210a87eae
Magne Alvheim <magne@alvheim.com>
2022-10-02 19:20:12 +0200

    java påbegynt

--- Templates/CSharp/source/MinApi/Program.cs
+++ Templates/CSharp/source/MinApi/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/welcome/{name}", (string name) => $"Hallo fra dotnet, faggrupper, {name} is Looking handsome today");
+app.MapGet("/welcome/{name}", (string name) => $"Hallo fra dotnet, faggrupper. {name} is Looking handsome today");
 
 app.MapGet("/", () => "Hello World!");
 
